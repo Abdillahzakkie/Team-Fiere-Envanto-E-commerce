00d02ce4db3cee5c5f996d26ecb1aecc1ef1b45c
Abdillahzakkie <zakariyyaopeyemi@gmail.com>
2020-06-20 15:07:20 +0100

    made some changes

--- js/myScript.js
+++ js/myScript.js
@@ -5,31 +5,24 @@ navToggle.addEventListener("click", () => links.classList.toggle("show-links"));
 
 // Slider
 const slider = document.querySelector('.slider-img');
-const slideBtn = [...document.querySelectorAll('.slider-btn')];
+const sliderBtn = [...document.querySelectorAll('.slider-btn')];
+// const sliderBtn = Array.from(document.querySelectorAll('.slider-btn'));
 
+// On load set default slider image
 window.addEventListener('DOMContentLoaded', () => slider.src = `./images/services-bg.jpg`);
 
-slideBtn.map(btn => {
+sliderBtn.map(btn => {
     let count = 0;
     const [img1, img2, img3] = ['services-bg.jpg', 'slide_02.jpg', 'slide_03.jpg'];
-
     const slideImage = [img1, img2, img3];
+
     btn.addEventListener('click', e => {
         const currentBtn = e.currentTarget.dataset.id;
-        if (currentBtn === 'prevBtn') {
-            if(count > 0) {
-                count--
-            } else {
-                count = slideImage.length - 1;
-            }
-        } else {
-            if (count < (slideImage.length - 1)) {
-                count++
-            } else {
-                count = 0
-            }
-        }
-        slider.src = `./images/${slideImage[count]}`
+        (currentBtn === 'prevBtn')
+            ? ( count > 0 ) ? count-- : count = (slideImage.length - 1)
+            : ( count < (slideImage.length - 1) ) ? count++ : count = 0;
+    
+        slider.src = `./images/${slideImage[count]}`;
     })
 })
 
